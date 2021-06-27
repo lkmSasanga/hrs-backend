f8c37e2963df61c2bbd3362a3f3428c01aaafc51
lkmSasanga <lkmsasanga@gmail.com>
2021-06-27 12:59:35 +0530

    removed other configs

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 const app = require("express")();
 const cors = require("cors");
 require("dotenv").config();
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
 
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-const connection = mongoose.connection;
-connection
-    .once("open", () => {
-        console.log("MongoDB database connection established successfully");
-    })
-    .on("error", function(err) {
-        console.log("Error", err);
-    });
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-});
+// const connection = mongoose.connection;
+// connection
+//     .once("open", () => {
+//         console.log("MongoDB database connection established successfully");
+//     })
+//     .on("error", function(err) {
+//         console.log("Error", err);
+//     });
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+// });
 
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-// app.use(app.json());
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
 
 app.get("/", function(req, res) {
     res.send("Welcome to API!");
 });
 
-let v1 = require("./routes");
+// let v1 = require("./routes");
 
-app.use("/", v1.router);
+// app.use("/", v1.router);
 
 app.use(function(req, res) {
     res.status(404).send({ url: req.originalUrl + " not found" });
